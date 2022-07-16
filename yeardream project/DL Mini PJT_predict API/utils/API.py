79d2bd3d7e3e205ea48df9f386e06cd707c281f1
baekyk <baekyk23@naver.com>
2022-07-17 01:07:59 +0900

    docs : modified

--- yeardream project/DL Mini PJT_predict API/utils/API.py
+++ yeardream project/DL Mini PJT_predict API/utils/API.py
@@ -10,5 +10,5 @@ enc1= OHE_fit(df[['Airline']])
 enc2= OHE_fit(df[['AirportFrom']])
 enc3= OHE_fit(df[['AirportTo']])
 
-model = model_load('Airline_delay/model/trained_model.h5')
-flask(enc1, enc2, enc3, model)
+bestmode = model_load()
+flask(enc1, enc2, enc3, bestmode)
